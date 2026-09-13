9933fab85e1f798a5123b1f93c3e90d2e1429b1a
LSton3y <leolstone@icloud.com>
2026-09-13 21:39:28 +0100

    add: Added license header to files

--- include/strutil/strutil.hpp
+++ include/strutil/strutil.hpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2026 LSton3y
+// SPDX-License-Identifier: MIT
+
 #pragma once
 
 #include <string>
--- tests/test_find.cpp
+++ tests/test_find.cpp
@@ -1,3 +1,6 @@
+// Copyright (c) 2026 LSton3y
+// SPDX-License-Identifier: MIT
+
 #include <gtest/gtest.h>
 #include "strutil/strutil.hpp"
 
